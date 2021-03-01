50326aad061d6a122629d1f2eb410008de84984f
Lario <osunkunleolanrewaju@gmail.com>
2021-03-01 20:30:32 +0100

    have been able to setup graphql schema and query it

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,31 +1,46 @@
 const graphql = require('graphql');
+const _ = require('lodash')
 
-const {GraphQLObjectType, GraphQLSring} = graphql;
+const {
+    GraphQLObjectType, 
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
+} = graphql;
+
+// Dummy data
+var books = [
+    {name: 'Name of the wind', genre: 'Fantasy', id: '1'},
+    {name: 'Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'Long Earth', genre: 'Sci-Fi', id: '3'}
+]
 
 // Definition of Types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLSring},
-        name: {type: GraphQLSring},
-        genre: {type: GraphQLSring}
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
     })
 })
 
-// Defining ReQueries
+// Defining RootQueries
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
         book:{
             type: BookType,
-            args: {id: {type: GraphQLSring}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
                 // code to get data from db and other source
+                return _.find(books, {id: args.id})
             }
         }
     }
 })
 
-module.exports = new graphql.GraphQLSchema({
+
+module.exports = new GraphQLSchema({
     query: RootQuery
 })
