a5be1bac5b452ff32056ed35e5324151cb0b34d5
Lario <osunkunleolanrewaju@gmail.com>
2021-03-02 21:23:20 +0100

    graphql server has been set to interract with mongodb

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 // // Dummy data
@@ -29,8 +30,6 @@ const {
 // ]
 
 // Definition of Types
-
-
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -71,8 +70,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type: AuthorType,
             args:{
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args){
                 let author = new Author({
@@ -85,9 +84,9 @@ const Mutation = new GraphQLObjectType({
         addBook:{
             type: BookType,
             args:{
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book = new Book({
