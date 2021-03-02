d2cb9cb2ab2710ec3f510aa020bbe61c9f599d78
Lario <osunkunleolanrewaju@gmail.com>
2021-03-02 13:45:23 +0100

    done setting up graphql server

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,14 +5,25 @@ const {
     GraphQLObjectType, 
     GraphQLString,
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 // Dummy data
 var books = [
-    {name: 'Name of the wind', genre: 'Fantasy', id: '1'},
-    {name: 'Final Empire', genre: 'Fantasy', id: '2'},
-    {name: 'Long Earth', genre: 'Sci-Fi', id: '3'}
+    {name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1'},
+    {name: 'Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    {name: 'Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'Hero of Ages', genre: 'Fantsay', id: '4', authorId: '2'},
+    {name: 'Color of Magic', genre: 'Fantsay', id: '5', authorId: '3'},
+    {name: 'Light Fantastic', genre: 'Fantsay', id: '6', authorId: '3'}
+]
+
+var authors = [
+    {name: 'Patric Rothfus', age:'44', id: '1'},
+    {name: 'Brandom Sanderson', age:'42', id: '2'},
+    {name: 'John Terry', age:'47', id: '3'}
 ]
 
 // Definition of Types
@@ -21,9 +32,32 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                return _.find(authors, {id: parent.authorId})
+            }
+        }
     })
 })
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, {authorId: parent.id})
+            }
+        }
+    })
+})
+
+
 
 // Defining RootQueries
 const RootQuery = new GraphQLObjectType({
@@ -35,6 +69,25 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 // code to get data from db and other source
                 return _.find(books, {id: args.id})
+            }
+        },
+        author:{
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                return _.find(authors, {id: args.id})
+            }
+        },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
+        },
+        authors:{
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
             }
         }
     }
