099ca8b61c41450dddebe7dcd5004953dde0141e
Lario <osunkunleolanrewaju@gmail.com>
2021-03-03 08:24:26 +0100

    set up the client side of the application

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BookList from './Component/BookList'
+
+function App() {
+  return (
+    <div className="app">
+      <h2>Lario Reading List</h2>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
